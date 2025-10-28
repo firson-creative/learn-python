c9779f543cdcb787aca275030361d59ef301ea45
firson-creative <firsoncreative@outlook.com>
2025-10-28 12:14:15 +0800

    first commit: latihan menggunakan variabel, fungsi input(), dan fungsi if-elif dalam program day1.py

---
+++ day1.py
@@ -0,0 +1,20 @@
+#Latihan menuliskan variabel, menggunakan input, dan fungsi if
+
+a = int(input('Nilai A = ')) # deklarasi variabel dan penggunaan statement input
+b = int(input('Nilai B = ')) 
+
+if (a > b):
+    print("A lebih besar daripada B.")
+    a = b
+    b = 2 * a
+
+elif (a < b):
+    print("A lebih kecil daripada B.")
+    b = a
+    a = 2 * b
+
+else:
+    print("A sama dengan B.")
+
+print("Nilai A: ", a)    
+print("Nilai B: ", b)    
